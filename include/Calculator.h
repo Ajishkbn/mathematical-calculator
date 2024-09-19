6554880b1502edb08cdca529193a45087829754e
Ajish K B <ajish.kbn@gmail.com>
2024-09-19 15:29:13 +0530

    feat: add multiply

--- include/Calculator.h
+++ include/Calculator.h
@@ -5,6 +5,7 @@ class Calculator {
 public:
     int add(int a, int b);
     int subract(int a, int b);
+    int multiply(int a, int b);
 };
 
 #endif // CALCULATOR_H
--- src/Calculator.cpp
+++ src/Calculator.cpp
@@ -7,3 +7,7 @@ int Calculator::add(int a, int b) {
 int Calculator::subract(int a, int b){
     return a - b;
 }
+
+int Calculator::multiply(int a, int b){
+    return a*b;
+}
